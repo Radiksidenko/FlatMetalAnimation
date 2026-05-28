d9f8699eaee357dbbecc02446bac69bcf2fb8cc3
Radomyr Sidenko <radik-sidehnko@ukr.net>
2026-05-28 16:44:02 +0300

    Initial Commit

---
+++ FlatAnimation/FlatAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlatAnimationApp.swift
+//  FlatAnimation
+//
+//  Created by Radomyr Sidenko on 28.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct FlatAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
